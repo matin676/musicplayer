c16d8b519238950cfc0735d095bc60fc8481c42a
Matin <matinimam77@gmail.com>
2022-08-13 14:29:37 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,128 @@
+let allMusic = [
+    {
+        name: "22",
+        artist: "Alex Goot Λ Friends",
+        img: "music-1",
+        src: "music-1",
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-2",
+        src: "music-2",
+    },
+    {
+        name: "Renegade",
+        artist: "Axwell Λ Ingrosso",
+        img: "music-3",
+        src: "music-3",
+    },
+    {
+        name: "Believer",
+        artist: "Imagine Dragons",
+        img: "music-4",
+        src: "music-4",
+    },
+    {
+        name: "Cartoon On & On",
+        artist: "NCS Release",
+        img: "music-5",
+        src: "music-5",
+    },
+    {
+        name: "Counting Stars",
+        artist: "Alex Goot",
+        img: "music-6",
+        src: "music-6",
+    },
+    {
+        name: "Levitating",
+        artist: "Dua Lipa",
+        img: "music-7",
+        src: "music-7",
+    },
+    {
+        name: "New Rules",
+        artist: "Dua Lipa",
+        img: "music-8",
+        src: "music-8",
+    },
+    {
+        name: "Burn",
+        artist: "Ellie Goulding",
+        img: "music-9",
+        src: "music-9",
+    },
+    {
+        name: "I like me Better",
+        artist: "Lauv",
+        img: "music-10",
+        src: "music-10",
+    },
+    {
+        name: "If Avengers theme was A Song",
+        artist: "Hanu Dixit",
+        img: "music-11",
+        src: "music-11",
+    },
+    {
+        name: "If Pirates of Carrabean theme was A Song",
+        artist: "Hanu Dixit",
+        img: "music-12",
+        src: "music-12",
+    },
+    {
+        name: "Dirty Hands",
+        artist: "Kendra Dantes",
+        img: "music-13",
+        src: "music-13",
+    },
+    {
+        name: "Legends never Die",
+        artist: "Against The Current",
+        img: "music-14",
+        src: "music-14",
+    },
+    {
+        name: "Sunflower",
+        artist: "Post Malone Λ Swae Lee",
+        img: "music-15",
+        src: "music-15",
+    },
+    {
+        name: "Scared of the Dark",
+        artist: "Lil Wayne Λ Ty Dolla $ign",
+        img: "music-16",
+        src: "music-16",
+    },
+    {
+        name: "Last One Standing",
+        artist: "Skylar Grey Λ Eminem",
+        img: "music-17",
+        src: "music-17",
+    },
+    {
+        name: "Something Just Like This",
+        artist: "Coldplay Λ The Chainsmokers",
+        img: "music-18",
+        src: "music-18",
+    },
+    {
+        name: "Crazy",
+        artist: "Beauz Λ Jvna",
+        img: "music-19",
+        src: "music-19",
+    },
+    {
+        name: "Sun Goes Down",
+        artist: "Jim Yosef Λ Roy Knox",
+        img: "music-20",
+        src: "music-20",
+    },
+    {
+        name: "Want me",
+        artist: "Jimmy Hardwind Λ Mike Archangelo",
+        img: "music-21",
+        src: "music-21",
+    }
+]
